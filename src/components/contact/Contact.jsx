af5819b59735aba7db5d75c866811288841586f0
kamalnath69 <kamalnath9669@gmail.com>
2024-05-24 07:48:04 +0530

    KL

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -37,24 +37,15 @@ const Contact = () => {
     <div className="contact">
         <div className="contact-col">
             <h3>Send us a message <img src={msg_icon} alt="" /></h3>
-            <p>Feel free to reach out through contact form or find our contact information below. Your feedback, questions, and suggestions are important to us as we strive to provide exceptional service to our university community.</p> 
+            <p>Dr. D. Prasantha Bharathi,
+Conference Chair- ICMST-2025,
+Assistant Professor (SL. Gr) of Mathematics,
+Sri Eshwar College of Engineering, Coimbatore 641202, Tamil Nadu, India.</p> 
             <ul>
-                <li><img src={mail_icon} alt="" />Contact@Shailesh.dev</li>
-                <li><img src={phone_icon} alt="" />+91 82207-55948</li>
-                <li><img src={location_icon} alt="" />Hubspire, Infopark, Kakkanad <br />Kochi, Kerala</li>
+                <li><img src={mail_icon} alt="" />icmst@sece.ac.in</li>
+                <li><img src={phone_icon} alt="" />+91 63811 38369.</li>
+                <li><img src={location_icon} alt="" />Sri Eshwar College of Engineering<br />Coimbatore, Tamil Nadu</li>
             </ul>
-        </div>
-        <div className="contact-col">
-            <form  onSubmit={onSubmit}>
-                <label>Your name</label>
-                <input type="text" name='name' placeholder='Enter your name' required/>
-                <label>Phone Number</label>
-                <input type="tel" name='phone' placeholder='Enter your mobile number' required/>
-                <label>Write your messages here</label>
-                <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
-                <button type='submit' className='btn dark-btn'>Submit now <img src={white_arrow} alt="" /></button>
-            </form>
-            <span>{result}</span>
         </div>
     </div>
   )
